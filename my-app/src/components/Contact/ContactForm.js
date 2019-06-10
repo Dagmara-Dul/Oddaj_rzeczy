b56ddeb3dd5db3c7038498fe5bf07b0029ac6950
Dag4 <dul.dagmara@gmail.com>
2019-06-10 19:11:36 +0200

    contact cont.

--- my-app/src/components/Contact/ContactForm.js
+++ my-app/src/components/Contact/ContactForm.js
@@ -9,18 +9,26 @@ export default class ContactForm extends Component{
     render(){
         return(
             <section className="contact-form">
-            Formularz kontaktowy
-                {/* <Container  className="contact-container">
+            
+                <Container  className="contact-form-container">
                     <Row>
-                        <Col sm={12} lg={4} >
-                             <h3>Skontaktuj się z nami</h3>
-                             <div className="contact-image">
-                             </div>
-                             <ContactForm />
+                        <Col >
+                            <form>
+                                <div className="contact-form-package">
+                                    <h4>Formularz kontaktowy</h4>
+                                    <div className="contact-inputs">
+                                        <input type="text" name="name" placeholder="Imię"></input>
+                                        <input type="email" name="email" placeholder="Email"></input>
+                                    </div>
+                                    <textarea placeholder="Wiadomość"></textarea>
+                                    <button type="submit">Wyślij</button>
+                                </div>
+                            </form>
+                            
                         </Col>
                         
                     </Row>
-                </Container> */}
+                </Container>
                 
             </section>
         )
