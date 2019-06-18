b9c6f31eae6afacdd2efa5fa6a3d259c6ace4b6d
Dag4 <dul.dagmara@gmail.com>
2019-06-18 23:41:13 +0200

    start section rwd

--- my-app/src/components/StartSection/ChooseOption.js
+++ my-app/src/components/StartSection/ChooseOption.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import { Link } from 'react';
 
 
 export default class ChooseOption extends Component{
     render(){
         return(
-            <section className="choose-option">
-                <div className="choose-option-div">
-                    <p><span>Zacznij pomagać!</span><span>Oddaj niechciane rzeczy w zaufane ręce</span></p>
-                    <div className="choose-option-border"></div>
-                    <div className="give-or-organise">
-                        <a href="#"><span> oddaj <br></br> rzeczy </span></a>
-                        <a href="#"><span> zorganizuj <br></br> zbiórkę</span></a>
+            <Row>
+                <Col xs={12} lg={{ span:5, offset:7}}  >
+                <section className="choose-option">
+                    <div className="choose-option-div-xs">
+                        <h3>Masz w domu rzeczy, z którymi nie wiesz co zrobić?</h3>
+                        <p>Oddaj je potrzebującym <br></br><span>- szybko i w zaufane ręce</span></p>
+                        <a href="#"><span>Załóż konto</span></a>
                     </div>
-                </div>
-            </section>
+                    <div className="choose-option-div">
+                        <p><span>Zacznij pomagać!</span><span>Oddaj niechciane rzeczy w zaufane ręce</span></p>
+                        <div className="choose-option-border"></div>
+                        <div className="give-or-organise">
+                            <a href="#"><span> oddaj <br></br> rzeczy </span></a>
+                            <a href="#"><span> zorganizuj <br></br> zbiórkę</span></a>
+                        </div>
+                    </div>
+                </section>
+                </Col>
+            </Row>
         )
     }
 }
--- my-app/src/components/StartSection/MainMenu.js
+++ my-app/src/components/StartSection/MainMenu.js
@@ -5,7 +5,14 @@ import * as Scroll from 'react-scroll';
 // import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTshirt } from '@fortawesome/free-solid-svg-icons';
+
+
+const hamburger = <FontAwesomeIcon icon={faBars} className="fa-2x" />
+const tShirt = <FontAwesomeIcon icon= { faTshirt } className="fa-2x" />
 
 export default class MainMenu extends Component{
 
@@ -15,7 +22,11 @@ export default class MainMenu extends Component{
         return(
             <section className="container main-menu">
                 <div className="main-menu-div">
-                <span className="fas fa-bars mobile-menu" id="main-menu-hamburger"></span>
+                {/* <span className="fas fa-bars mobile-menu" id="main-menu-hamburger"></span> */}
+                    <div className="main-menu-div-sm">
+                        <span id='main-menu-logo'>{ tShirt }</span>
+                        <span id="main-menu-hamburger">{hamburger}</span>
+                    </div>
                     <ul className="main-menu-navbar">
                         <li><Link activeClass="active" to="start" spy={true} smooth={true} offset={0} duration={600} ><span>Start</span></Link></li>
                         <li><Link activeClass="active" to="what-is-it-about" spy={true} smooth={true} offset={0} duration={600} ><span>O co chodzi?</span></Link></li>
