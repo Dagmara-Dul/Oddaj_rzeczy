5a9d17e78d933814b2fcefa39424e000bce4543b
Dag4 <dul.dagmara@gmail.com>
2019-07-04 11:22:09 +0200

    Updates

---
+++ precache-manifest.05cec163976ebffe64b18e9834069f7e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a3f473fe14c4bb197682aeb8e768a",
+    "url": "/Oddaj_rzeczy/index.html"
+  },
+  {
+    "revision": "d1c2ed14d9d4b2779cfd",
+    "url": "/Oddaj_rzeczy/static/css/main.ac9f4365.chunk.css"
+  },
+  {
+    "revision": "e21c20c889f95c2a2569",
+    "url": "/Oddaj_rzeczy/static/js/2.49d43bb4.chunk.js"
+  },
+  {
+    "revision": "d1c2ed14d9d4b2779cfd",
+    "url": "/Oddaj_rzeczy/static/js/main.f0264288.chunk.js"
+  },
+  {
+    "revision": "c7c918c9c5b22c0de2df",
+    "url": "/Oddaj_rzeczy/static/js/runtime~main.4b340b29.js"
+  },
+  {
+    "revision": "0e48fc7f4f6fe85f5d1a34bdb30a84af",
+    "url": "/Oddaj_rzeczy/static/media/about.0e48fc7f.jpg"
+  },
+  {
+    "revision": "d0e9ee92a6398e6b31b1515c2ec6ab5e",
+    "url": "/Oddaj_rzeczy/static/media/accessories-accessory-boots-322207.d0e9ee92.jpg"
+  },
+  {
+    "revision": "d830f4fcf168916a2059125f47313d43",
+    "url": "/Oddaj_rzeczy/static/media/art-background-blank-1020317.d830f4fc.jpg"
+  },
+  {
+    "revision": "74c241482c275f28baa081947a4616db",
+    "url": "/Oddaj_rzeczy/static/media/footer_bg_img.74c24148.jpg"
+  },
+  {
+    "revision": "2c60c6fe9605a0b26170b834fdc6244f",
+    "url": "/Oddaj_rzeczy/static/media/header_start_img.2c60c6fe.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Oddaj_rzeczy/static/media/logo.5d5d9eef.svg"
+  }
+]);
